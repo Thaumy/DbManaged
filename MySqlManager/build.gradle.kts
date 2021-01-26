35de05aa35e8f43d5a69771d81852651cc612bb8
Thaumy Cheng <Studio_ai@outlook.com>
2021-01-26 21:30:52 +0800

    ~~

--- MySqlManager/build.gradle.kts
+++ MySqlManager/build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     kotlin("jvm") version "1.4.21"
 }
 
+group = "thaumy.cn"
+version = "1.0"
+
 repositories {
     mavenCentral()
 }
@@ -10,30 +13,4 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     testCompile("junit", "junit", "4.12")
-}
-
-group = "thaumy.cn"
-version = "1.0"
-val main_class = "MySqlManager.MySqlManagerKt"
-
-tasks.withType<Jar> {
-    manifest {
-        attributes["Main-Class"] = main_class
-    }
-
-    from(sourceSets.main.get().output)
-
-    dependsOn(configurations.runtimeClasspath)
-    from({
-        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-    })
-}
-
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
 }
