e03cce127abe94a462a9c56ed9f8197671822aae
Thaumy Cheng <Studio_ai@outlook.com>
2021-01-30 23:59:30 +0800

    修复了连接池索引溢出BUG

--- MySqlManager/src/main/kotlin/MySqlManager/MySqlManager.kt
+++ MySqlManager/src/main/kotlin/MySqlManager/MySqlManager.kt
@@ -12,10 +12,10 @@ class MySqlManager() {
         get() {
             /* 在连接数超出时检查无用连接并进行清理 */
             if (ConnectionPool.count() > 32) {
-                for (i in ConnectionPool.count() downTo 0) {
+                for (i in ConnectionPool.count() - 1 downTo 0) {
                     /* 如果连接中断或是关闭（这都是不工作的状态） *///JDBC貌似没有给出数据库连接的状态枚举信息
                     if (ConnectionPool[i].isClosed) {
-                        ConnectionPool[i].close();/* 注销并移除连接池 *///JDBC没有注销和关闭的概念区分，仅有关闭
+                        //ConnectionPool[i].close();/* 注销并移除连接池 *///JDBC没有注销和关闭的概念区分，仅有关闭
                         ConnectionPool.removeAt(i);
                     }
                 }
@@ -31,15 +31,6 @@ class MySqlManager() {
         ConnectionPool = mutableListOf()
     }
 
-/*    constructor(MySqlConnMsg: MySqlConnMsg) : this() {
-        ConnectionString =
-            "jdbc:mysql://${MySqlConnMsg.DataSource}:${MySqlConnMsg.Port}" +
-                    "/?user=${MySqlConnMsg.User}&" +
-                    "password=${MySqlConnMsg.PWD}&" +
-                    "UseAffectedRows=TRUE;"
-        *//* UPDATE语句返回受影响的行数而不是符合查询条件的行数 *//*
-    }*/
-
     constructor(MySqlConnMsg: MySqlConnMsg, Database: String) : this() {
         ConnectionString =/* USING目标数据库 */
             "jdbc:mysql://${MySqlConnMsg.DataSource}:${MySqlConnMsg.Port}" +
@@ -50,7 +41,7 @@ class MySqlManager() {
 
     //一次性连接使用器
     inline fun <T> DoInConnection(todo: (Connection) -> T): T {
-        val conn: Connection = Connection;
+        val conn = Connection;
         val result: T = todo(conn);
         conn.close()
         return result
