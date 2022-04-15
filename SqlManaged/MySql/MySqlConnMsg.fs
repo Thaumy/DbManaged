4155226f6d14a93ea4d2621582a489f1e6484d86
Thaumy <Studio_ai@outlook.com>
2022-04-16 00:51:27 +0800

    interface constraints

--- SqlManaged/MySql/MySqlConnMsg.fs
+++
@@ -1,7 +0,0 @@
-namespace MySqlManaged
-
-type MySqlConnMsg =
-    { DataSource: string
-      Port: uint16
-      User: string
-      Password: string }
--- SqlManaged/PgSql/PgSqlConnMsg.fs
+++
@@ -1,7 +0,0 @@
-namespace PgSqlManaged
-
-type PgSqlConnMsg =
-    { Host: string
-      Port: uint16
-      User: string
-      Password: string }
